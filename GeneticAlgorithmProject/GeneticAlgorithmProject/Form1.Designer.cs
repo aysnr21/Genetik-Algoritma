714eaff36ac604158d6be5c87a177c64a267581a
ayşenur_aslan <118816997+aysnr21@users.noreply.github.com>
2025-04-20 18:58:46 +0300

    Add files via upload
    🧬 Genetik Algoritma ile Sürekli Optimizasyon Problemi Çözümü (C# - Windows Form App)
    Bu proje, genetik algoritmanın tüm aşamaları C# programlama dili ile ve Windows Forms arayüzü kullanılarak geliştirilmiştir. Algoritma parametreleri dinamik olarak girilebilir ve sonuçlar görsel olarak analiz edilebilir.
    
    
    Genetik Algoritma Aşamaları:
    - Başlangıç popülasyonu oluşturma
    - Uygunluk (fitness) hesaplama
    - Seçim (Selection)
    - Çaprazlama (Crossover)
    - Mutasyon (Mutation)
    - Seçkinlik (Elitizm)
    - Jenerasyon güncelleme ve yakınsama takibi
    - Sonuçların görselleştirilmesi
    
    🔢 Test Problemi:
    Problem türü: Sürekli optimizasyon & minimizasyon problemi
    Kodlama türü: Reel kodlama 
    
    🖥️ Windows Forms Arayüzü:
    Kullanıcının aşağıdaki parametreleri arayüz üzerinden girebilmesi sağlanmıştır:
    * Popülasyon Boyutu
    * Çaprazlama Oranı
    * Mutasyon Oranı
    * Seçkinlik Oranı veya Seçkin Birey Sayısı
    * Jenerasyon Sayısı
    
    📊 Sonuç ve Görselleştirme:
    En iyi çözümün parametre değerleri ve amaç fonksiyonu değeri kullanıcıya gösterilir.
    Jenerasyonlara bağlı olarak yakınsama (convergence) grafiği çizdirilir.
    
    
    Kodlama Nesne Yönelimli Programlama (OOP) prensiplerine uygun olarak yapılandırılmıştır.

---
+++ GeneticAlgorithmProject/GeneticAlgorithmProject/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace GeneticAlgorithmProject
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtPopulationSize;
+        private System.Windows.Forms.TextBox txtCrossoverRate;
+        private System.Windows.Forms.TextBox txtMutationRate;
+        private System.Windows.Forms.TextBox txtElitismCount;
+        private System.Windows.Forms.TextBox txtGenerations;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblBestSolution;
+        private System.Windows.Forms.Label lblBestFitness;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+
+        // Dispose method to clean up components
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        // Initialize components of the form
+        private void InitializeComponent()
+        {
+            this.txtPopulationSize = new System.Windows.Forms.TextBox();
+            this.txtCrossoverRate = new System.Windows.Forms.TextBox();
+            this.txtMutationRate = new System.Windows.Forms.TextBox();
+            this.txtElitismCount = new System.Windows.Forms.TextBox();
+            this.txtGenerations = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblBestSolution = new System.Windows.Forms.Label();
+            this.lblBestFitness = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+
+            // txtPopulationSize
+            this.txtPopulationSize.Location = new System.Drawing.Point(150, 20);
+            this.txtPopulationSize.Name = "txtPopulationSize";
+            this.txtPopulationSize.Size = new System.Drawing.Size(100, 20);
+            this.txtPopulationSize.TabIndex = 0;
+
+            // txtCrossoverRate
+            this.txtCrossoverRate.Location = new System.Drawing.Point(150, 50);
+            this.txtCrossoverRate.Name = "txtCrossoverRate";
+            this.txtCrossoverRate.Size = new System.Drawing.Size(100, 20);
+            this.txtCrossoverRate.TabIndex = 1;
+
+            // txtMutationRate
+            this.txtMutationRate.Location = new System.Drawing.Point(150, 80);
+            this.txtMutationRate.Name = "txtMutationRate";
+            this.txtMutationRate.Size = new System.Drawing.Size(100, 20);
+            this.txtMutationRate.TabIndex = 2;
+
+            // txtElitismCount
+            this.txtElitismCount.Location = new System.Drawing.Point(150, 110);
+            this.txtElitismCount.Name = "txtElitismCount";
+            this.txtElitismCount.Size = new System.Drawing.Size(100, 20);
+            this.txtElitismCount.TabIndex = 3;
+
+            // txtGenerations
+            this.txtGenerations.Location = new System.Drawing.Point(150, 140);
+            this.txtGenerations.Name = "txtGenerations";
+            this.txtGenerations.Size = new System.Drawing.Size(100, 20);
+            this.txtGenerations.TabIndex = 4;
+
+            // label1 (Population Size)
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Popülasyon Boyutu";
+
+            // label2 (Crossover Rate)
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Çaprazlama Oranı";
+
+            // label3 (Mutation Rate)
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Mutasyon Oranı";
+
+            // label4 (Elitism Count)
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Seçkinlik Oranı";
+
+            // label5 (Generations Count)
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Jenerasyon Sayısı";
+
+            // btnStart (Start Button)
+            this.btnStart.Location = new System.Drawing.Point(150, 180);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 30);
+            this.btnStart.TabIndex = 10;
+            this.btnStart.Text = "Başlat";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+
+            // lblBestSolution (Best Solution Label)
+            this.lblBestSolution.AutoSize = true;
+            this.lblBestSolution.Location = new System.Drawing.Point(20, 230);
+            this.lblBestSolution.Name = "lblBestSolution";
+            this.lblBestSolution.Size = new System.Drawing.Size(0, 13);
+            this.lblBestSolution.TabIndex = 11;
+
+            // lblBestFitness (Best Fitness Label)
+            this.lblBestFitness.AutoSize = true;
+            this.lblBestFitness.Location = new System.Drawing.Point(20, 260);
+            this.lblBestFitness.Name = "lblBestFitness";
+            this.lblBestFitness.Size = new System.Drawing.Size(0, 13);
+            this.lblBestFitness.TabIndex = 12;
+
+            // chart1 (Chart Control)
+            this.chart1.Location = new System.Drawing.Point(300, 20);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(400, 300);
+            this.chart1.TabIndex = 13;
+            this.chart1.Text = "chart1";
+
+            // Form1 (Form Settings)
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 350);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.lblBestFitness);
+            this.Controls.Add(this.lblBestSolution);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtGenerations);
+            this.Controls.Add(this.txtElitismCount);
+            this.Controls.Add(this.txtMutationRate);
+            this.Controls.Add(this.txtCrossoverRate);
+            this.Controls.Add(this.txtPopulationSize);
+            this.Name = "Form1";
+            this.Text = "Genetik Algoritma Optimizasyonu";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ GeneticAlgorithmProject/GeneticAlgorithmProject/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace GeneticAlgorithmProject
+{
+    public partial class Form1 : Form
+    {
+        private Random _random;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeChart();
+            _random = new Random();
+        }
+
+        private void InitializeChart()
+        {
+            chart1.Series.Clear();
+
+            // ChartArea ekleyin
+            chart1.ChartAreas.Add(new ChartArea());
+
+            var series = new Series("Fitness");
+            series.ChartType = SeriesChartType.Line;
+            series.XValueType = ChartValueType.Int32;  // X ekseni için integer değerler
+            series.YValueType = ChartValueType.Double;  // Y ekseni için double değerler
+            chart1.Series.Add(series);
+
+            // Eksen etiketleri ve başlıklar
+            chart1.ChartAreas[0].AxisX.Title = "Jenerasyonlar";
+            chart1.ChartAreas[0].AxisY.Title = "Fitness Değeri";
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int populationSize = int.Parse(txtPopulationSize.Text);
+                double crossoverRate = double.Parse(txtCrossoverRate.Text);
+                double mutationRate = double.Parse(txtMutationRate.Text);
+                int elitismCount = int.Parse(txtElitismCount.Text);
+                int generations = int.Parse(txtGenerations.Text);
+
+                // Rosenbrock Fonksiyonu
+                Func<double[], double> fitnessFunction = (genes) =>
+                {
+                    double sum = 0;
+                    for (int i = 0; i < genes.Length - 1; i++)
+                    {
+                        sum += 100 * Math.Pow(genes[i + 1] - Math.Pow(genes[i], 2), 2) + Math.Pow(genes[i] - 1, 2);
+                    }
+                    return sum;
+                };
+
+                // Genetik algoritma başlatma
+                GeneticAlgorithm ga = new GeneticAlgorithm(populationSize, 2, crossoverRate, mutationRate, elitismCount, fitnessFunction, -5, 5);
+
+                chart1.Series["Fitness"].Points.Clear();
+                for (int i = 0; i < generations; i++)
+                {
+                    ga.Evolve();
+
+                    // Fitness değerini kontrol etmek
+                    Console.WriteLine($"Jenerasyon {i}: En iyi Fitness = {ga.GetBestIndividual().Fitness}");
+
+                    chart1.Series["Fitness"].Points.AddXY(i, ga.GetBestIndividual().Fitness);
+
+                    // Label'leri güncelle
+                    lblBestSolution.Text = $"En İyi Çözüm: x = {ga.GetBestIndividual().Genes[0]:F4}, y = {ga.GetBestIndividual().Genes[1]:F4}";
+                    lblBestFitness.Text = $"En İyi Fitness: {ga.GetBestIndividual().Fitness:F4}";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Hata: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+
+    public class Individual
+    {
+        public double[] Genes { get; set; }
+        public double Fitness { get; set; }
+
+        public Individual(int geneLength, double minValue, double maxValue, Random random)
+        {
+            Genes = new double[geneLength];
+            for (int i = 0; i < geneLength; i++)
+            {
+                Genes[i] = random.NextDouble() * (maxValue - minValue) + minValue;
+            }
+            Fitness = 0;
+        }
+
+        public void CalculateFitness(Func<double[], double> fitnessFunction)
+        {
+            Fitness = fitnessFunction(Genes);
+        }
+    }
+
+    public class Population
+    {
+        public List<Individual> Individuals { get; set; }
+
+        public Population(int populationSize, int geneLength, double minValue, double maxValue, Random random)
+        {
+            Individuals = new List<Individual>();
+            for (int i = 0; i < populationSize; i++)
+            {
+                Individuals.Add(new Individual(geneLength, minValue, maxValue, random));
+            }
+        }
+
+        public Individual GetFittest()
+        {
+            return Individuals.OrderBy(i => i.Fitness).First();
+        }
+    }
+
+    public class GeneticAlgorithm
+    {
+        private Population _population;
+        private double _crossoverRate;
+        private double _mutationRate;
+        private int _elitismCount;
+        private Func<double[], double> _fitnessFunction;
+        private double _minValue;
+        private double _maxValue;
+        private Random _random;
+
+        public GeneticAlgorithm(int populationSize, int geneLength, double crossoverRate, double mutationRate, int elitismCount, Func<double[], double> fitnessFunction, double minValue, double maxValue)
+        {
+            _random = new Random();
+            _population = new Population(populationSize, geneLength, minValue, maxValue, _random);
+            _crossoverRate = crossoverRate;
+            _mutationRate = mutationRate;
+            _elitismCount = elitismCount;
+            _fitnessFunction = fitnessFunction;
+            _minValue = minValue;
+            _maxValue = maxValue;
+
+            // Fitness değerlerini hesapla
+            foreach (var individual in _population.Individuals)
+            {
+                individual.CalculateFitness(_fitnessFunction);
+            }
+        }
+
+        public void Evolve()
+        {
+            var newPopulation = new List<Individual>();
+
+            // Elitizm: En iyi bireyleri yeni popülasyona ekle
+            var elites = _population.Individuals.OrderBy(i => i.Fitness).Take(_elitismCount).ToList();
+            newPopulation.AddRange(elites);
+
+            // Yeni popülasyonu oluştur
+            while (newPopulation.Count < _population.Individuals.Count)
+            {
+                var parent1 = SelectParent();
+                var parent2 = SelectParent();
+
+                if (_random.NextDouble() < _crossoverRate)
+                {
+                    var (child1, child2) = Crossover(parent1, parent2);
+                    newPopulation.Add(child1);
+                    newPopulation.Add(child2);
+                }
+                else
+                {
+                    newPopulation.Add(parent1);
+                    newPopulation.Add(parent2);
+                }
+            }
+
+            // Mutasyon uygula
+            foreach (var individual in newPopulation)
+            {
+                Mutate(individual);
+            }
+
+            // Yeni popülasyon ile eski popülasyonu değiştir
+            _population.Individuals = newPopulation;
+
+            // Fitness hesaplamayı güncelle
+            foreach (var individual in _population.Individuals)
+            {
+                individual.CalculateFitness(_fitnessFunction);
+            }
+        }
+
+        private Individual SelectParent()
+        {
+            var tournamentSize = 5;
+            var tournament = _population.Individuals.OrderBy(x => _random.Next()).Take(tournamentSize).ToList();
+            return tournament.OrderBy(i => i.Fitness).First();
+        }
+
+        private (Individual, Individual) Crossover(Individual parent1, Individual parent2)
+        {
+            var child1 = new Individual(parent1.Genes.Length, _minValue, _maxValue, _random);
+            var child2 = new Individual(parent2.Genes.Length, _minValue, _maxValue, _random);
+
+            for (int i = 0; i < parent1.Genes.Length; i++)
+            {
+                if (_random.NextDouble() < 0.5)
+                {
+                    child1.Genes[i] = parent1.Genes[i];
+                    child2.Genes[i] = parent2.Genes[i];
+                }
+                else
+                {
+                    child1.Genes[i] = parent2.Genes[i];
+                    child2.Genes[i] = parent1.Genes[i];
+                }
+            }
+
+            return (child1, child2);
+        }
+
+        private void Mutate(Individual individual)
+        {
+            for (int i = 0; i < individual.Genes.Length; i++)
+            {
+                if (_random.NextDouble() < _mutationRate)
+                {
+                    individual.Genes[i] = _random.NextDouble() * (_maxValue - _minValue) + _minValue;
+                }
+            }
+        }
+
+        public Individual GetBestIndividual()
+        {
+            return _population.GetFittest();
+        }
+    }
+}
